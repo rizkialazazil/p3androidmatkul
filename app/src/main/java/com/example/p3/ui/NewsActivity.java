b858cac56e75f3a1ab8f75f40cebcfe529d2cbd8
rizkialazazil <Rizkialazazil@gmail.com>
2025-06-26 08:07:12 +0700

    commit pertama

---
+++ app/src/main/java/com/example/p3/ui/NewsActivity.java
@@ -0,0 +1,47 @@
+package com.example.p3.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.p3.R;
+import com.example.p3.util.Constans;
+import com.squareup.picasso.Picasso;
+
+public class NewsActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private ImageView imgDetail;
+    private TextView txtTitleDetail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            v.setPadding(insets.getSystemWindowInsetLeft(),
+                    insets.getSystemWindowInsetTop(),
+                    insets.getSystemWindowInsetRight(),
+                    insets.getSystemWindowInsetBottom());
+            return insets;
+        });
+        imgDetail = findViewById(R.id.imgDetail);
+        txtTitleDetail = findViewById(R.id.txtTitleDetail);
+        mWebView = findViewById(R.id.webData);
+
+        // Load title
+        txtTitleDetail.setText(Constans.TITLE);
+
+        // Load image
+        Picasso.get().load(Constans.IMAGE).into(imgDetail);
+
+        // Load description
+        mWebView.loadDataWithBaseURL(null,
+                "<head><style>img{max-width: 90%; width:auto; height:auto;}</style></head>"
+                        + Constans.DESCRIPTION, "text/html", "UTF-8", null);
+    }
+}
---
+++ app/src/main/java/com/example/p3/util/Constans.java
@@ -0,0 +1,7 @@
+package com.example.p3.util;
+
+public class Constans {
+    public static String TITLE;
+    public static String IMAGE;
+    public static String DESCRIPTION;
+}
